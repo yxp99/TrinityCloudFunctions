3eed68741057fcfefff418c81bbed3093e44b801
Shadow Song <shadowsong@Shadows-Mac-mini.local>
2018-12-31 00:16:02 -0500

    Major update include cloud function

--- firecast/functions/src/index.ts
+++ firecast/functions/src/index.ts
@@ -8,20 +8,7 @@
 //  //  	response.send("Hello from Firebase!");
 //  // });
 
-// // [START makeUppercase]
-// // Listens for new messages added to /messages/:pushId/original and creates an
-// // uppercase version of the message to /messages/:pushId/uppercase
-// exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-//     .onCreate((snapshot, context) => {
-//       // Grab the current value of what was written to the Realtime Database.
-//       const original = snapshot.val();
-//       console.log('Uppercasing', context.params.pushId, original);
-//       const uppercase = original.toUpperCase();
-//       // You must return a Promise when performing asynchronous tasks inside a Functions such as
-//       // writing to the Firebase Realtime Database.
-//       // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-//       return snapshot.ref.parent.child('uppercase').set(uppercase);
-//     });
+
 
 /**
  * Copyright 2016 Google Inc. All Rights Reserved.
@@ -47,39 +34,175 @@ const functions = require('firebase-functions');
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
+
+
 admin.initializeApp();
+
+// Get a database reference to our posts
+const db = admin.database();
 // [END import]
 
-// [START addMessage]
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-// [START addMessageTrigger]
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-// [END addMessageTrigger]
-  // Grab the text parameter.
-  const original = req.query.text;
-  // [START adminSdkPush]
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  const snapshot = await admin.database().ref('/messages').push({original: original});
-  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-  res.redirect(303, snapshot.ref.toString());
-  // [END adminSdkPush]
-});
-// [END addMessage]
+// // [START addMessage]
+//IMPORTANT, THIS PART IS USED FOR HTTP CONNECTION TEST. CODE IS GOOD. BUT NOT USEFUL HERE. 
+// // Take the text parameter passed to this HTTP endpoint and insert it into the
+// // Realtime Database under the path /messages/:pushId/original
+// // [START addMessageTrigger]
+// exports.addMessage = functions.https.onRequest(async (req, res) => {
+// // [END addMessageTrigger]
+//   // Grab the text parameter.
+//   const original = req.query.text;
+//   // [START adminSdkPush]
+//   // Push the new message into the Realtime Database using the Firebase Admin SDK.
+//   const snapshot = await admin.database().ref('/messages').push({original: original});
+//   // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+//   res.redirect(303, snapshot.ref.toString());
+//   // [END adminSdkPush]
+// });
+// // [END addMessage]
 
 // [START makeUppercase]
 // Listens for new messages added to /messages/:pushId/original and creates an
 // uppercase version of the message to /messages/:pushId/uppercase
+
 exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
     .onCreate((snapshot, context) => {
+
+      
+
+
       // Grab the current value of what was written to the Realtime Database.
       const original = snapshot.val();
+
+      //write Cloud Function Console Log, this line does not affect this function
       console.log('Uppercasing', context.params.pushId, original);
+
+      //Transfer to upper case. 
       const uppercase = original.toUpperCase();
       // You must return a Promise when performing asynchronous tasks inside a Functions such as
       // writing to the Firebase Realtime Database.
       // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
       return snapshot.ref.parent.child('uppercase').set(uppercase);
     });
-// [END makeUppercase]
-// [END all]
+
+
+
+
+//[START Cloud Timer]
+exports.myCloudTimer = functions.database.ref('/DB1_0/Game/game(idFDFDS)/CurrentQuestion/countDown').onCreate((snapshot, context) => {
+       return db.ref('/DB1_0/Game/game(idFDFDS)/CurrentQuestion/content').once('value', snap => {
+        if (!snap.exists()) {
+            return Promise.reject('time is not defined in the database.');
+        }
+
+
+        const timeInSeconds = snapshot.val();
+        console.log('Cloud Timer was Started: ' + timeInSeconds);
+
+        return functionTimer(timeInSeconds,
+            elapsedTime => {
+                db.ref('/DB1_0/Game/game(idFDFDS)/CurrentQuestion/countDown').set(elapsedTime);
+            })
+            .then(totalTime => {
+                console.log('Timer of ' + totalTime + ' has finished.');
+
+                // [Remove Current Question]
+                db.ref('/DB1_0/Game/game(idFDFDS)/CurrentQuestion').remove()
+                  .then(function() {
+                    console.log("Remove succeeded.")
+                  })
+                  .catch(function(error) {
+                    console.log("Remove failed: " + error.message)
+                  });
+                 // [Remove ends]
+
+                const questionListRef = db.ref('/DB1_0/Game/game(idFDFDS)');
+                const newQuestionRef = questionListRef.push();
+                //newQuestionRef.child('content').set (snap.val());
+                newQuestionRef.set({
+                  'content': snap.val(),
+                  'answer': 'notSet'
+                });
+
+            })
+            // .then(() => new Promise(resolve => setTimeout(resolve, 1000)))
+            // .then(() => event.data.ref.remove())
+            .catch(error => console.error(error));
+    });
+});
+
+
+function functionTimer (seconds, call) {
+    return new Promise((resolve, reject) => {
+        if (seconds > 300) {
+            reject('execution would take too long...');
+            return;
+        }
+        const interval = setInterval(onInterval, 1000);
+        let elapsedSeconds = 0;
+
+        function onInterval () {
+            if (elapsedSeconds >= seconds) {
+                clearInterval(interval);
+                call(0);
+                resolve(elapsedSeconds);
+                return;
+            }
+            call(seconds - elapsedSeconds);
+            elapsedSeconds++;
+        }
+    });
+}
+//[END Cloud Timer]
+
+
+
+
+
+
+// DO NOT DELETE THIS SECTION! 
+//[Original Timer from stackOverflow]  
+// exports.myCloudTimer = functions.database.ref('/startTimerRequest/').onCreate((event) => {
+//     return db.ref('cloudTimer/timeInMs').once('value', snap => {
+//         if (!snap.exists()) {
+//             return Promise.reject('time is not defined in the database.');
+//         }
+
+//         let timeInSeconds = snap.val() / 1000;
+//         console.log('Cloud Timer was Started: ' + timeInSeconds);
+
+//         return functionTimer(timeInSeconds,
+//             elapsedTime => {
+//                 db.ref('cloudTimer/observableTime').set(elapsedTime);
+//             })
+//             .then(totalTime => {
+//                 console.log('Timer of ' + totalTime + ' has finished.');
+//             })
+//             .then(() => new Promise(resolve => setTimeout(resolve, 1000)))
+//             .then(() => event.data.ref.remove())
+//             .catch(error => console.error(error));
+//     });
+// });
+
+// function functionTimer (seconds, call) {
+//     return new Promise((resolve, reject) => {
+//         if (seconds > 300) {
+//             reject('execution would take too long...');
+//             return;
+//         }
+//         let interval = setInterval(onInterval, 1000);
+//         let elapsedSeconds = 0;
+
+//         function onInterval () {
+//             if (elapsedSeconds >= seconds) {
+//                 clearInterval(interval);
+//                 call(0);
+//                 resolve(elapsedSeconds);
+//                 return;
+//             }
+//             call(seconds - elapsedSeconds);
+//             elapsedSeconds++;
+//         }
+//     });
+// }
+//[Original Timer from stackOverflow Ends]  
+
